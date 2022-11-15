35d9e498e8f6affdbc87b467b4368a24a1abb889
Tsotne Nazarashvili <cotne.95@gmail.com>
2022-11-15 12:26:50 +0400

    add cypress

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:8000',
+    specPattern: "cypress/e2e",
+    supportFile: false
+  }
+})
