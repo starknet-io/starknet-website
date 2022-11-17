1ca738090fc388e985813d49e6fc219b7fdd59e3
Tsotne Nazarashvili <cotne.95@gmail.com>
2022-11-18 00:24:44 +0400

    work in progress

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,20 +1,63 @@
 import { GatsbyNode } from "gatsby";
+import { createFilePath } from "gatsby-source-filesystem";
 import path from "path";
+
+const defaultLanguage = "en";
+
+const commonRedirectProps = {
+  isPermanent: true,
+  ignoreCase: true,
+  force: true,
+};
+
+export const onCreateNode: GatsbyNode<{
+  fileAbsolutePath: string;
+}>["onCreateNode"] = async ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === "Mdx") {
+    let slug = createFilePath({ node, getNode, basePath: "content/posts" });
+    let lang = defaultLanguage;
+
+    if (/^[/]\w{2}[/]/.test(slug)) {
+      lang = slug.split("/")[1];
+    } else {
+      slug = `/${defaultLanguage}${slug}`;
+    }
+
+    createNodeField({
+      node,
+      name: "lang",
+      value: lang,
+    });
+  }
+};
 
 export const createPages: GatsbyNode["createPages"] = async ({
   graphql,
   actions,
   reporter,
 }) => {
-  // const { createPage } = actions;
-  // const postTemplate = path.resolve("./src/templates/Post.tsx")
-  
+  // const { createPage, createRedirect, createSlice } = actions;
+
+  // createRedirect({
+  //   ...commonRedirectProps,
+  //   fromPath: "/",
+  //   toPath: `/${defaultLanguage}`,
+  // });
+
+  // const postTemplate = path.resolve("./src/templates/Post.tsx");
+
   // const result = await graphql<any>(`
   //   {
   //     posts: allFile(filter: { sourceInstanceName: { eq: "posts" } }) {
   //       nodes {
   //         childMdx {
   //           id
+  //           fields {
+  //             locale
+  //             isDefault
+  //           }
   //           frontmatter {
   //             slug
   //           }
@@ -31,20 +74,36 @@ export const createPages: GatsbyNode["createPages"] = async ({
   //   reporter.panicOnBuild("Error loading MDX result", result.errors);
   // }
 
-  // // Create blog post pages.
   // const posts = result.data.posts.nodes;
 
-  // // you'll call `createPage` for each result
-  // posts.forEach((node: any) => {
-  //   createPage({
-  //     // As mentioned above you could also query something else like frontmatter.title above and use a helper function
-  //     // like slugify to create a slug
-  //     path: node.frontmatter.slug,
-  //     // Provide the path to the MDX content file so webpack can pick it up and transform it into JSX
-  //     component: `${postTemplate}?__contentFilePath=${node.internal.contentFilePath}`,
-  //     // You can use the values in this context in
-  //     // our page layout component
-  //     context: { id: node.id },
-  //   });
+  // posts.foreach(({ childMdx: node }: any) => {
+  //   return [
+  //     `mdx-${node.id}`,
+  //     createSlice({
+  //       id: `blog-${node.fields.locale}-${}`,
+  //       component: node.internal.contentFilePath,
+  //     }),
+  //   ];
+  // });
+
+  // posts.forEach(({ childMdx: node }: any) => {
+  //   if (node.fields.isDefault) {
+  //     createRedirect({
+  //       ...commonRedirectProps,
+  //       fromPath: node.frontmatter.slug,
+  //       toPath: `${defaultLanguage}/${node.frontmatter.slug}`,
+  //     });
+
+  //     createPage({
+  //       // As mentioned above you could also query something else like frontmatter.title above and use a helper function
+  //       // like slugify to create a slug
+  //       path: node.frontmatter.slug,
+  //       // Provide the path to the MDX content file so webpack can pick it up and transform it into JSX
+  //       component: postTemplate,
+  //       // You can use the values in this context in
+  //       // our page layout component
+  //       context: { slug: node.frontmatter.slug },
+  //     });
+  //   }
   // });
 };
--- src/templates/Post.tsx
+++ src/templates/Post.tsx
@@ -1,29 +1,47 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Slice } from "gatsby";
 import { MDXProvider } from "@mdx-js/react";
 import { Link } from "gatsby";
 
 const shortcodes = { Link };
 
 interface Props {
-  readonly children: React.ReactNode
-  readonly data: any
+  readonly children: React.ReactNode;
+  readonly data: any;
 }
 
 export default function PageTemplate({ data, children }: Props) {
+  if (data.mdx == null) {
+    return <div>This page hasn't been translated yet</div>;
+  }
+
   return (
     <>
       <h1>{data.mdx.frontmatter.title}</h1>
-      <MDXProvider components={shortcodes}>{children}</MDXProvider>
+      <MDXProvider components={shortcodes}>
+        <Slice
+          alias={`blog-${
+            data.mdx.fields.locale
+          }-${data.mdx.frontmatter.slug.replace(/\//g, "--")}`}
+        />
+      </MDXProvider>
     </>
   );
 }
 
 export const query = graphql`
-  query ($id: String!) {
-    mdx(id: { eq: $id }) {
+  query ($locale: String!, $slug: String!) {
+    mdx(
+      fields: { locale: { eq: $locale } }
+      frontmatter: { slug: { eq: $slug } }
+    ) {
       frontmatter {
+        slug
         title
+      }
+      fields {
+        locale
+        isDefault
       }
     }
   }
