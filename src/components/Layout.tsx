b40aa6dd01f8e10a9843a1376903dbc7cfde35cb
Tsotne Nazarashvili <cotne.95@gmail.com>
2022-11-18 14:48:45 +0400

    Ft/locales (#3)

---
+++ src/components/Layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { IntlProvider } from "react-intl";
+import { LocaleProvider } from "gatsby-theme-i18n";
+
+interface Props {
+  readonly children: React.ReactNode;
+  readonly pageContext: {
+    readonly locale: string;
+  };
+}
+
+export function DocumentLayout({ children, pageContext }: Props) {
+  const locale = pageContext.locale;
+  const messages = require(`../../i18n/intl/${locale}.json`);
+
+  return (
+    <LocaleProvider pageContext={pageContext}>
+      <IntlProvider locale={locale} key={locale} messages={messages}>
+        {children}
+      </IntlProvider>
+    </LocaleProvider>
+  );
+}
---
+++ src/templates/Post.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { graphql, Slice } from "gatsby";
+
+interface Props {
+  readonly children: React.ReactNode;
+  readonly data: any;
+}
+
+export default function PageTemplate({ data, children }: Props) {
+  if (data.mdx == null) {
+    return <div>This page hasn't been translated yet</div>;
+  }
+
+  return (
+    <>
+      <h1>{data.mdx.frontmatter.title}</h1>
+
+      <Slice alias='content-mdx' allowEmpty={true} />
+    </>
+  );
+}
+
+export const query = graphql`
+  query ($locale: String!, $slug: String!) {
+    mdx(
+      fields: { locale: { eq: $locale } }
+      frontmatter: { slug: { eq: $slug } }
+    ) {
+      frontmatter {
+        slug
+        title
+      }
+      fields {
+        locale
+        isDefault
+      }
+    }
+  }
+`;
