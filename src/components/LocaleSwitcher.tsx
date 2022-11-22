2d173fe06459f7ba6072b8ebefcb666cd52f9e2d
Tsotne Nazarashvili <cotne.95@gmail.com>
2022-11-22 12:40:36 +0400

    update LocaleSwitcher

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -11,13 +11,12 @@ export default function LocaleSwitcher() {
   const { locale } = useLocalization();
   const localeConfig = i18nConfig.find((c) => c.code === locale);
   const location = useLocation();
-  console.log(location);
 
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
-          {localeConfig?.localName}
+          {localeConfig?.name}
           <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
       </div>
@@ -33,15 +32,15 @@ export default function LocaleSwitcher() {
       >
         <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
-            {i18nConfig.map((c) => (
-              <Menu.Item>
+            {i18nConfig.map((c, i) => (
+              <Menu.Item key={i}>
                 {({ active }) => (
                   <LocalizedLink
                     className={classnames(
                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
                       "block px-4 py-2 text-sm",
                     )}
-                    to={location.pathname}
+                    to={location.pathname.replace(/^\/\w{2}\//, '/')}
                     language={c.code}
                   >
                     {c.name}
