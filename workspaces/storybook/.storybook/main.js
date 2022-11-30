e65206c3bcdb1901655942f9798cbba06e8ddfc3
Tsotne Nazarashvili <cotne.95@gmail.com>
2022-11-30 23:42:15 +0400

    update storybook config

--- workspaces/storybook/.storybook/main.js
+++ workspaces/storybook/.storybook/main.js
@@ -1,15 +1,28 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config) => {
+    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+    config.module.rules[0].exclude = [
+      /node_modules\/(?!(gatsby|gatsby-script)\/)/,
+    ];
+
+    // Remove core-js to prevent issues with Storybook
+    config.module.rules[0].exclude = [/core-js/];
+    // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+    config.module.rules[0].use[0].options.plugins.push(
+      require.resolve("babel-plugin-remove-graphql-queries")
+    );
+
+    config.resolve.mainFields = ["browser", "module", "main"];
+    return config;
+  },
+};
