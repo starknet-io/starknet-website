d972ee87ed596b926ad740d8b512f07950cd5216
Rob Walsh <r.walsh.21@gmail.com>
2022-12-02 13:42:30 +0000

    tabs component

---
+++ workspaces/storybook/src/stories/Tabs.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta } from "@storybook/react";
+import React from "react";
+
+import Tabs from "../../../../src/components/Tabs/Tabs";
+
+export default {
+  title: "Tabs",
+  component: Tabs.Root,
+} as ComponentMeta<typeof Tabs.Root>;
+
+export const Primary = () => (
+  <div>
+    <Tabs.Root defaultValue="tab1">
+      <Tabs.List aria-label="tabs example">
+        <Tabs.Trigger title="Page One" value="tab1" />
+        <Tabs.Trigger title="Page Two" value="tab2" />
+        <Tabs.Trigger title="Page three" value="tab3" />
+      </Tabs.List>
+      <Tabs.Content value="tab1">Tab one content</Tabs.Content>
+      <Tabs.Content value="tab2">Tab two content</Tabs.Content>
+      <Tabs.Content value="tab3">Tab three content</Tabs.Content>
+    </Tabs.Root>
+  </div>
+);
+Primary.storyName = "Default";
