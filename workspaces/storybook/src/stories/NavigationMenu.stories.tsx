0078b2be6cfadbb9acfeb6a8e75443370e50f28b
Rob Walsh <r.walsh.21@gmail.com>
2022-12-09 08:18:28 +0000

    navigation menu example

---
+++ workspaces/storybook/src/stories/NavigationMenu.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta } from "@storybook/react";
+import React from "react";
+import { NavigationMenu } from "@ui/NavigationMenu";
+import Wrapper from "./Wrapper";
+
+export default {
+  title: "starknet.io/NavigationMenu",
+  component: NavigationMenu,
+} as ComponentMeta<typeof NavigationMenu>;
+
+export const Primary = () => (
+  <div>
+    <div
+      style={{
+        backgroundColor: "red",
+        padding: 100,
+        display: "flex",
+        width: "50%",
+      }}
+    >
+      <NavigationMenu />
+    </div>
+  </div>
+);
+Primary.storyName = "Default";
