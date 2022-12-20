f64d2002453b3bba2ae1da9323cb39b2578d7715
shaojiang <caishaojiang@gmail.com>
2022-12-20 15:31:26 +0800

    Storybook: StyleGuide

--- workspaces/storybook/src/stories/Wrapper.tsx
+++ workspaces/storybook/src/stories/Wrapper.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React from 'react';
+import cx from 'classnames';
 
 type Props = {
+  className?: string;
   children: React.ReactNode;
 };
 
-const Wrapper = ({ children }: Props) => {
+const Wrapper = ({ className = '', children }: Props) => {
   return (
-    <div className="bg-body p-4 w-1/2 flex gap-4 flex-wrap">{children}</div>
+    <div className={cx('bg-body p-4 w-1/2 flex gap-4 flex-wrap', className)}>
+      {children}
+    </div>
   );
 };
 
