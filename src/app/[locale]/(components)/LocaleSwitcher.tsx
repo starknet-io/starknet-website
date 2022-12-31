d4986cdf3ff06f2b4a942fd54f55128ec108edcf
Tsotne Nazarashvili <cotne.95@gmail.com>
2022-12-31 16:18:02 +0400

    update i18n

--- src/app/[locale]/(components)/LocaleSwitcher.tsx
+++ src/app/[locale]/(components)/LocaleSwitcher.tsx
@@ -1,23 +1,23 @@
 import { Fragment } from "react";
 import React from "react";
 import classnames from "classnames";
-import i18nConfig from "i18n/config.json";
 import { Menu, Transition } from "src/libs/headlessui";
 import { ChevronDownIcon } from "src/libs/heroicons/20/solid";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { useLocale } from "next-intl";
+import { locales } from "src/data/i18n";
 
 export default function LocaleSwitcher() {
   const locale = useLocale();
-  const localeConfig = i18nConfig.find((c) => c.code === locale);
+  const localeConfig = locales.find((c) => c.code === locale)!;
   const pathname = usePathname()!;
 
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
-          {localeConfig?.name}
+          {localeConfig.name}
           <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
       </div>
@@ -33,13 +33,13 @@ export default function LocaleSwitcher() {
       >
         <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
-            {i18nConfig.map((c, i) => (
+            {locales.map((c, i) => (
               <Menu.Item key={i}>
                 {({ active }) => (
                   <Link
                     className={classnames(
                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "block px-4 py-2 text-sm",
+                      "block px-4 py-2 text-sm"
                     )}
                     href={`/${c.code}${pathname.replace(/^\/\w{2}\/?/, "/")}`}
                   >
--- src/data/i18n.ts
+++ src/data/i18n.ts
@@ -1,9 +1,33 @@
 import { AbstractIntlMessages } from "next-intl";
+import { NextIntlRuntimeConfig } from "next-intl/dist/server/NextIntlConfig";
+import { NextIntlConfig } from "next-intl";
+import i18nConfig from "i18n/config.json";
 
-export async function getI18n(locale: string): Promise<AbstractIntlMessages> {
+export async function getMessages(
+  runtimeConfig: NextIntlRuntimeConfig
+): Promise<AbstractIntlMessages> {
   try {
-    return (await import(`i18n/intl/${locale}.json`)).default;
+    return (await import(`i18n/intl/${runtimeConfig.locale}.json`)).default;
   } catch {}
 
   return (await import("i18n/intl/en.json")).default;
 }
+
+export interface LocaleConfig {
+  readonly code: string;
+  readonly hrefLang: string;
+  readonly name: string;
+  readonly localName: string;
+  readonly langDir: string;
+  readonly dateFormat: string;
+}
+
+export const locales: readonly LocaleConfig[] = i18nConfig
+
+export const config: NextIntlConfig = {
+  locales: i18nConfig.map((c) => c.code),
+  defaultLocale: "en",
+  getMessages,
+};
+
+export default config
