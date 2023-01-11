341f895c27c79eee9abda6e1ae48f1083f24761e
Rob Walsh <r.walsh.21@gmail.com>
2023-01-11 12:07:23 +0000

    Fix post pages

--- src/app/[locale]/posts/(components)/PostsPage.tsx
+++ src/app/[locale]/posts/(components)/PostsPage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SimpleGrid } from "@chakra-ui/react";
+import { Box, Container, Flex, SimpleGrid } from "@chakra-ui/react";
 import { ArticleCard } from "@ui/ArticleCard/ArticleCard";
 import { useMemo } from "react";
 import algoliasearch from "src/libs/algoliasearch/lite";
@@ -14,7 +14,8 @@ import {
   Configure,
   HitsPerPage,
 } from "src/libs/react-instantsearch-hooks-web";
-
+import { useHits } from "react-instantsearch-hooks";
+import { PageContentContainer } from "../../(components)/PageContentContainer";
 export interface AutoProps {
   readonly params: {
     readonly locale: string;
@@ -34,38 +35,53 @@ export function PostsPage({ params, env }: Props): JSX.Element | null {
   }, [env.ALGOLIA_APP_ID, env.ALGOLIA_SEARCH_API_KEY]);
 
   return (
-    <div className="mx-auto  max-w-7xl px-2 sm:px-4 lg:px-8 pt-7">
+    <PageContentContainer>
       <InstantSearch searchClient={searchClient} indexName="web_posts_dev">
         <Configure
           hitsPerPage={40}
           facetsRefinements={{ locale: [params.locale] }}
         />
-        {/* <SearchBox /> */}
-        <RefinementList attribute="brand" />
+        <Container>
+          <RefinementList attribute="brand" />
+        </Container>
+        <CustomHits />
 
-        <SimpleGrid
-          columns={{ base: 1, md: 2, lg: 2, xl: 2 }}
-          rowGap={{ base: "8", md: "12" }}
-          columnGap="8"
-          pt={8}
-        >
-          <Hits hitComponent={Hit} />
-        </SimpleGrid>
         {/* <Pagination /> */}
       </InstantSearch>
-    </div>
+    </PageContentContainer>
   );
 }
 
-function Hit({ hit }: any) {
+type HitProps = {
+  locale: string;
+  id: string;
+  short_desc: string;
+  category: string;
+  title: string;
+};
+
+function CustomHits() {
+  const { hits } = useHits();
+
   return (
-    <ArticleCard
-      publishedAt="May 20 · 5 min read"
-      excerpt="Cairo is a new open source design system for Figma and Sketch. It’s a collection of components, design patterns, and resources that you can use to design, prototype, and build products."
-      category={hit.category}
-      img="https://www.billboard.com/wp-content/uploads/media/st-vincent-escape-room-2017-billboard-1548.jpg?w=942&h=623&crop=1"
-      title={hit.title}
-      id={hit.id}
-    />
+    <SimpleGrid
+      columns={{ base: 1, md: 2, lg: 2, xl: 2 }}
+      rowGap={{ base: "8", md: "12" }}
+      columnGap="8"
+      pt={8}
+    >
+      {hits.map((hit, i) => (
+        <ArticleCard
+          href={`/${hit.locale}/posts/${hit.id}`}
+          publishedAt="May 20 · 5 min read"
+          excerpt={hit.short_desc}
+          category="engineering"
+          img="https://miro.medium.com/max/1400/1*W1dObj1HvyUqVB2z60Ug_A.webp"
+          title={hit?.title}
+          id="id"
+          key={i}
+        />
+      ))}
+    </SimpleGrid>
   );
 }
