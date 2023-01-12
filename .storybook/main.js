974f276be67e6126240b4ea5742bbb1e645efe02
Rob Walsh <r.walsh.21@gmail.com>
2023-01-12 23:38:13 +0000

    Add storybook beta to project

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": {
+    "name": "@storybook/nextjs",
+    "options": {}
+  },
+  "docs": {
+    "autodocs": "tag"
+  }
+}
