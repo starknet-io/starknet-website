17b4ed26db32c61853a1b6ad9dafe2412ace4d0c
Tsotne Nazarashvili <cotne.95@gmail.com>
2023-01-16 22:25:39 +0400

    dsds

--- src/app/[locale]/[page]/page.tsx
+++ src/app/[locale]/[page]/page.tsx
@@ -1,19 +1,16 @@
 import { use } from "react";
+import { getMessages } from "src/data/i18n/intl";
 import { getPageByFilename } from "src/data/pages";
-import { useMessages } from "../(components)/ClientLocaleProvider";
 
 export interface Props {
-  readonly params: {
-    readonly locale: string;
+  readonly params: LocaleParams & {
     readonly page: string;
   };
 }
 
-export default function Page({ params }: Props): JSX.Element {
-  const messages = useMessages()
-  const { title, MDXContent } = use(
-    getPageByFilename(params.page, params.locale)
-  );
+export default function Page({ params: { locale, page } }: Props): JSX.Element {
+  const messages = use(getMessages(locale));
+  const { title, MDXContent } = use(getPageByFilename(page, locale));
 
   return (
     <div className="mx-auto  max-w-7xl px-2 sm:px-4 lg:px-8 pt-7">
