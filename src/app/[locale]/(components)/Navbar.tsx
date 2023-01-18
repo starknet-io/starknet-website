2e4998d9d57b4f2db954da70f21e5a862903b441
Tsotne Nazarashvili <cotne.95@gmail.com>
2023-01-18 19:03:57 +0400

    dsds

--- src/app/[locale]/(components)/Navbar.tsx
+++ src/app/[locale]/(components)/Navbar.tsx
@@ -9,6 +9,7 @@ import { NavbarContainer } from "@ui/Layout/Navbar/NavbarContainer";
 import { NavLink } from "@ui/Layout/Navbar/NavLink";
 import { Text } from "@ui/Typography/Text";
 import { useLocale } from "./ClientLocaleProvider";
+import Link from "next/link";
 
 export interface Props {
   readonly mainMenu: MainMenu;
@@ -19,7 +20,68 @@ export default function Navbar({ mainMenu }: Props) {
 
   return (
     <NavbarContainer>
-      <NavBar
+      <ul>
+        {mainMenu.items.map((mainMenuItem, mainMenuItemIndex) => (
+          <li key={mainMenuItemIndex}>
+            <span>{mainMenuItem.title}</span>
+
+            {mainMenuItem.columns?.length && (
+              <ul>
+                {mainMenuItem.columns?.map((column, columnIndex) => (
+                  <li key={columnIndex}>
+                    <ul>
+                      {column.blocks?.map((block, blockIndex) => (
+                        <li key={blockIndex}>
+                          <span>{block.title}</span>
+
+                          <ul>
+                            {block.items?.map((item, itemIndex) => {
+                              let title =
+                                item.custom_title ||
+                                item.page_title ||
+                                item.post_title;
+
+                              let link;
+
+                              if (item.custom_external_link) {
+                                link = item.custom_external_link;
+                              } else if (item.custom_internal_link) {
+                                link = `/${locale}/${item.custom_internal_link.replace(
+                                  /(^\/|\/$)/g,
+                                  "",
+                                )}`;
+                              } else if (item.page) {
+                                link = `/${locale}/pages/${item.page.replace(
+                                  /(^\/|\/$)/g,
+                                  "",
+                                )}`;
+                              } else if (item.post) {
+                                link = `/${locale}/posts/${item.post.replace(
+                                  /(^\/|\/$)/g,
+                                  "",
+                                )}`;
+                              } else {
+                                return <span key={itemIndex}>{title}</span>;
+                              }
+
+                              return (
+                                <Link href={link} key={itemIndex}>
+                                  {title}
+                                </Link>
+                              );
+                            })}
+                          </ul>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
+      {/* <NavBar
         languageSwitcher={<LocaleSwitcher />}
         desktopNavItems={
           <>
@@ -67,7 +129,7 @@ export default function Navbar({ mainMenu }: Props) {
             })}
           </NavAccordian.Root>
         }
-      />
+      /> */}
     </NavbarContainer>
   );
 }
