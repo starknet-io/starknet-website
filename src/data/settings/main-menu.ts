ad3d9b030e00c629a8857da80a0928424cb80b7e
Tsotne Nazarashvili <cotne.95@gmail.com>
2023-01-18 18:05:41 +0400

    asasa

--- src/data/settings/main-menu.ts
+++ src/data/settings/main-menu.ts
@@ -1,22 +1,43 @@
 import { defaultLocale } from "../i18n/config";
 import { getFirst, getJSON } from "../utils";
 
-export type Page = {
-  readonly page: string;
-  readonly title: string;
-  readonly pages: readonly Page[];
-};
-
 export interface MainMenu {
-  readonly pages: readonly Page[];
+  readonly items: MainMenuItem[];
+}
+
+export interface MainMenuItem {
+  readonly title: string;
+  readonly columns?: Column[];
+}
+
+export interface Column {
+  readonly blocks?: Block[];
+}
+
+export interface Block {
+  readonly title?: string;
+  readonly items?: BlockItem[] | null;
+}
+
+export interface BlockItem {
+  readonly custom_title?: string;
+  readonly custom_icon?: string;
+  readonly custom_internal_link?: string;
+  readonly custom_external_link?: string;
+
+  readonly page?: string;
+  readonly page_title?: string;
+
+  readonly post?: string;
+  readonly post_title?: string;
 }
 
 export async function getMainMenu(locale: string): Promise<MainMenu> {
   try {
-    return (await getFirst(
+    return await getFirst(
       () => getJSON(`_dynamic/main-menu/${locale}.json`),
       () => getJSON(`_dynamic/main-menu/${defaultLocale}.json`),
-    )) as Page;
+    );
   } catch (cause) {
     throw new Error("getMainMenu failed!", {
       cause,
