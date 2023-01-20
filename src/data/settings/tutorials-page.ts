8c5d19cfe097068fb241157228e8c2c9a1785770
Tsotne Nazarashvili <cotne.95@gmail.com>
2023-01-20 13:14:25 +0400

    fix typo

--- src/data/settings/tutorials-page.ts
+++ src/data/settings/tutorials-page.ts
@@ -1,12 +1,12 @@
 import { defaultLocale } from "../i18n/config";
 import { getFirst, getYAML } from "../utils";
 
-export interface EventsPage {
+export interface TutorialsPage {
   readonly title: string;
   readonly description: string;
 }
 
-export async function getTutorialsPage(locale: string): Promise<EventsPage> {
+export async function getTutorialsPage(locale: string): Promise<TutorialsPage> {
   try {
     return await getFirst(
       () => getYAML(`settings/${locale}/tutorials-page.yml`),
