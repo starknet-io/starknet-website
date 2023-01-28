64aa84248a8cd595cb745ff97b54797d07cab50a
Rob Walsh <r.walsh.21@gmail.com>
2023-01-28 12:31:14 +0000

    update post post layout

--- src/app/[locale]/posts/[category]/[slug]/page.tsx
+++ src/app/[locale]/posts/[category]/[slug]/page.tsx
@@ -1,8 +1,10 @@
+import { PageLayout } from "@ui/Layout/PageLayout";
+import { Heading } from "@ui/Typography/Heading";
 import { notFound } from "next/navigation";
 import { PageContentContainer } from "src/app/[locale]/(components)/PageContentContainer";
 import { Block } from "src/blocks/Block";
 import { getPostBySlug } from "src/data/posts";
-import { Flex } from "src/libs/chakra-ui";
+import { Container, Flex, Img } from "src/libs/chakra-ui";
 
 export interface Props {
   readonly params: {
@@ -15,19 +17,26 @@ export default async function Page({
   params: { slug, locale },
 }: Props): Promise<JSX.Element> {
   try {
-    const { title, blocks, body } = await getPostBySlug(slug, locale);
+    const { title, blocks, body, image } = await getPostBySlug(slug, locale);
 
     return (
-      <PageContentContainer>
-        <h2>{title}</h2>
+      <PageLayout
+        main={
+          <Container maxWidth="846px">
+            <Img borderRadius={"8px"} src={image} alt={title} />
+            <Heading variant="h2" as="h2" color="heading-navy-fg">
+              {title}
+            </Heading>
 
-        <Flex direction="column" gap="32px">
-          {blocks.map((block, i) => (
-            <Block key={i} block={block} locale={locale} />
-          ))}
-          <Block block={{ type: "markdown", body }} locale={locale} />
-        </Flex>
-      </PageContentContainer>
+            <Flex direction="column" gap="32px">
+              {blocks.map((block, i) => (
+                <Block key={i} block={block} locale={locale} />
+              ))}
+              <Block block={{ type: "markdown", body }} locale={locale} />
+            </Flex>
+          </Container>
+        }
+      />
     );
   } catch (err) {
     console.log(err);
