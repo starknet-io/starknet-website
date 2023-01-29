3dce2d435ec8c284a261e7544db40fcf53a0d00c
Tsotne Nazarashvili <cotne.95@gmail.com>
2023-01-29 20:17:05 +0400

    fix post data

--- src/app/[locale]/posts/[category]/[slug]/page.tsx
+++ src/app/[locale]/posts/[category]/[slug]/page.tsx
@@ -32,16 +32,7 @@ export default async function Page({
   params: { slug, locale },
 }: Props): Promise<JSX.Element> {
   try {
-    const {
-      title,
-      blocks,
-      image,
-      topic,
-      post_type,
-      published_date,
-      time_to_consume,
-      category,
-    } = await getPostBySlug(slug, locale);
+    const post = await getPostBySlug(slug, locale);
 
     return (
       <PageLayout
@@ -56,13 +47,13 @@ export default async function Page({
             </BreadcrumbItem>
             <BreadcrumbItem>
               <BreadcrumbLink fontSize="sm" href="#" noOfLines={1}>
-                {category}
+                {post.category}
               </BreadcrumbLink>
             </BreadcrumbItem>
 
             <BreadcrumbItem isCurrentPage>
               <BreadcrumbLink fontSize="sm" noOfLines={1}>
-                {title}
+                {post.title}
               </BreadcrumbLink>
             </BreadcrumbItem>
           </Breadcrumb>
@@ -70,36 +61,41 @@ export default async function Page({
         pageLastUpdated={"Page last updated 21 Nov 2023"}
         main={
           <Container maxWidth="846px">
-            <Img mb="32px" borderRadius={"8px"} src={image} alt={title} />
+            <Img
+              mb="32px"
+              borderRadius={"8px"}
+              src={post.image}
+              alt={post.title}
+            />
             <Box mb={"16px"}>
               <Badge variant="stark_at_home" textTransform="capitalize">
-                {post_type}
+                {post.post_type}
               </Badge>
             </Box>
             <Heading variant="h2" as="h2" color="heading-navy-fg">
-              {title}
+              {post.title}
             </Heading>
             <Flex mt="16px">
               <HStack>
                 <Text fontSize="sm" color="muted">
-                  {published_date} ·
+                  {post.published_date} ·
                 </Text>
                 <Text fontSize="sm" color="muted">
-                  {time_to_consume}
+                  {post.time_to_consume}
                 </Text>
               </HStack>
               <Spacer />
             </Flex>
             <Divider mt="8px" mb="32px" />
             <Flex direction="column" gap="32px">
-              {blocks.map((block, i) => (
+              {post.blocks.map((block, i) => (
                 <Block key={i} block={block} locale={locale} />
               ))}
             </Flex>
             <Spacer height="32px" />
             <Divider />
             <Flex direction="row" gap="8px" mt="64px">
-              {topic.map((item, i) => (
+              {post.topic.map((item, i) => (
                 <Tag key={i}> {item} </Tag>
               ))}
             </Flex>
