129b4f551ec1e9015ef2464142ddf0a25b7b3f39
Rob Walsh <r.walsh.21@gmail.com>
2023-02-05 10:48:54 +0200

    add another card style

--- src/style/tokens.ts
+++ src/style/tokens.ts
@@ -177,6 +177,8 @@ export const tokens = {
       "gradient-cyan-b": "  #CCF6E3",
       "gradient-pink-a": " #FFDAD7 ",
       "gradient-pink-b": "  #E8D4E3",
+      "gradient-blue-default-a": " #E9F2F8 ",
+      "gradient-blue-default-b": "  #FBF6F1",
     },
 
     dark: {
@@ -356,6 +358,8 @@ export const tokens = {
       "gradient-cyan-b": "  #1B2034",
       "gradient-pink-a": " #191E31 ",
       "gradient-pink-b": "  #223033",
+      "gradient-blue-default-a": " #252525 ",
+      "gradient-blue-default-b": "  #1B1B1B",
     },
   },
 };
@@ -1040,6 +1044,10 @@ export const semanticTokens = {
       default: tokens.colors.light["gradient-pink-a"],
       _dark: tokens.colors.dark["gradient-pink-a"],
     },
+    "gradient-blue-default-a": {
+      default: tokens.colors.light["gradient-pink-a"],
+      _dark: tokens.colors.dark["gradient-pink-a"],
+    },
     "gradient-orange-b": {
       default: tokens.colors.light["gradient-orange-b"],
       _dark: tokens.colors.dark["gradient-orange-b"],
@@ -1061,6 +1069,10 @@ export const semanticTokens = {
       _dark: tokens.colors.dark["gradient-peach-b"],
     },
     "gradient-pink-b": {
+      default: tokens.colors.light["gradient-pink-b"],
+      _dark: tokens.colors.dark["gradient-pink-b"],
+    },
+    "gradient-default-blue-b": {
       default: tokens.colors.light["gradient-pink-b"],
       _dark: tokens.colors.dark["gradient-pink-b"],
     },
