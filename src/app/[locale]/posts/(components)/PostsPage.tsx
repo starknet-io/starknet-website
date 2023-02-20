ba2bbe24022789101b0e642bcc867978ee7e9b8f
Rob Walsh <r.walsh.21@gmail.com>
2023-02-20 19:27:13 +0000

    add topics heading to posts

--- src/app/[locale]/posts/(components)/PostsPage.tsx
+++ src/app/[locale]/posts/(components)/PostsPage.tsx
@@ -27,6 +27,7 @@ import { PageLayout } from "@ui/Layout/PageLayout";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import type { Topic } from "src/data/topics";
 import { useInfiniteHits } from "react-instantsearch-hooks-web";
+import { Heading } from "@ui/Typography/Heading";
 
 export interface Props extends LocaleProps {
   readonly categories: readonly Category[];
@@ -86,6 +87,9 @@ export function PostsPage({
           }
           leftAside={
             <Box minH="xs" display={{ base: "none", lg: "block" }}>
+              <Heading mt="48px" color="heading-navy-fg" as="h4" variant="h4">
+                Topics
+              </Heading>
               <CustomTopics topics={topics} />
             </Box>
           }
