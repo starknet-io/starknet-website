cbd74fa4d985175028c0950e414e341b0721c5ef
Tsotne Nazarashvili <cotne.95@gmail.com>
2023-03-15 13:44:50 +0400

    normalize yml before uploading

---
+++ workspaces/content-onedge/src/pre-crowdin.ts
@@ -0,0 +1,46 @@
+import fs, { mkdir } from "node:fs/promises";
+import * as path from "node:path";
+import * as dotenv from "dotenv";
+import YAML from "yaml";
+
+process.chdir(path.resolve(__dirname, "../../.."));
+
+const dotenvFiles = [".env.local", ".env"];
+
+dotenvFiles.forEach((path) => dotenv.config({ path }));
+
+import { yaml } from "./utils";
+import { defaultLocale } from "./locales";
+
+const resources = [
+  "categories",
+  "events",
+  "jobs",
+  "pages",
+  "posts",
+  "settings",
+  "topics",
+  "tutorials",
+];
+
+for (const resourceName of resources) {
+  const locale = defaultLocale;
+  const filenames = await fs.readdir(`_data/${resourceName}/${locale}`);
+
+  for (const filename of filenames) {
+    const filepath = path.join("_data", resourceName, locale, filename);
+
+    const data = await yaml(filepath);
+
+    await fs.writeFile(filepath, YAML.stringify(data, {
+      strict: true,
+      collectionStyle: 'block',
+      defaultStringType: 'QUOTE_DOUBLE',
+      defaultKeyType: 'PLAIN',
+      doubleQuotedAsJSON: true,
+      version: '1.2'
+    }), {
+      encoding: "utf8",
+    });
+  }
+}
