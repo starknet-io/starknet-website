6d31511ee7b83efb983abfe2e6aad4a04f3558c5
AkromDev <126797224+lorcan-codes@users.noreply.github.com>
2023-03-30 21:30:41 +0500

    Remove custom useWindowSize hook

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
 import Page from "./[...slug]/page";
 
 export default async function Index({
   params: { locale },
 }: LocaleProps): Promise<JSX.Element> {
-  return <Page params={{ locale, slug: ["home"] }} />;
+  return (
+    <div
+      style={{
+        display: "grid",
+      }}
+    >
+      <Link href="/en/video/prover">Video page</Link>
+      <Link href="/video-embed">Embed</Link>
+    </div>
+  );
 }
