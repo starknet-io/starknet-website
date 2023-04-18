2c81154f0030dacdfc4b226f42eee24e3fe98bb6
Milos Lekovic <milos@yuki.wtf>
2023-04-18 22:10:57 +0200

    Removing redundant

--- src/components/Link/CustomLink.tsx
+++ src/components/Link/CustomLink.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { Link, LinkOverlay, LinkProps } from "@chakra-ui/react";
 import NextLink from "next/link";
-import { useColorModeValue } from "@chakra-ui/react"
+
 type Props = LinkProps & { size?: string };
 
 export const CustomLink = (props: Props) => {
-  const bg = useColorModeValue('red.500', 'red.200')
 
   return (
     <LinkOverlay>
