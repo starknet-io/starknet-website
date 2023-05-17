4a9337c584d0bb337942bcfa729bc96a14a8b95e
AkromDev <126797224+lorcan-codes@users.noreply.github.com>
2023-05-17 16:00:34 +0500

    Fix live preview failing in create tuts, events and jobs cards

--- src/app/[locale]/jobs/(components)/JobsCard.tsx
+++ src/app/[locale]/jobs/(components)/JobsCard.tsx
@@ -4,19 +4,19 @@ import { JobsHit } from "./JobsPage";
 
 export default function JobsCard({ hit }: { hit: JobsHit }) {
   let tags: string[] = [];
-  if (hit.job.role) tags.push(hit.job.role);
-  if (hit.job.type) tags.push(hit.job.type);
-  if (hit.job.location) tags.push(hit.job.location);
+  if (hit.job?.role) tags.push(hit.job.role);
+  if (hit.job?.type) tags.push(hit.job.type);
+  if (hit.job?.location) tags.push(hit.job.location);
 
   return (
     <ListCard
       variant="job"
-      startDateTime={hit.contact.name}
-      image={hit.contact.logo}
-      title={hit.job.title}
-      description={hit.job.description}
+      startDateTime={hit.contact?.name}
+      image={hit.contact?.logo}
+      title={hit.job?.title}
+      description={hit.job?.description}
       type={tags}
-      href={hit.job.apply_url}
+      href={hit.job?.apply_url || ""}
     />
   );
 }
