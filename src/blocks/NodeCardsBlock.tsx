22cd0724eee64793707017bea5a54c7aa9c20866
Milos Lekovic <milos@yuki.wtf>
2023-05-19 11:12:18 +0200

    Fixing deploy error

--- src/blocks/NodeCardsBlock.tsx
+++ src/blocks/NodeCardsBlock.tsx
@@ -37,7 +37,7 @@ Props): JSX.Element {
   return (
     <Box>
       <Container maxW="1062px">
-        <Heading color="heading-navy-fg" variant="h3" mb="10px" id={`toc-${slugify(title)}`}>{title}</Heading>
+        <Heading color="heading-navy-fg" variant="h3" mb="10px" id={`toc-${slugify(title || "")}`}>{title}</Heading>
         <Text variant="body" mb="24px">{description}</Text>
         <Flex gap={4} direction="column" flex={1}>
           {nodes.map((node, i) => {
