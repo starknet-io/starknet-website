ffb002ecfd0133859fcc2e2fa0600a569af47166
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2023-05-25 16:56:47 +0500

    Serverside render tutorial video page && filter locale

--- src/app/[locale]/tutorials/video/[id]/page.tsx
+++ src/app/[locale]/tutorials/video/[id]/page.tsx
@@ -1,126 +1,18 @@
-"use client";
-import {
-  BreadcrumbItem,
-  BreadcrumbLink,
-  Breadcrumb,
-  Box,
-  Flex
-} from "@chakra-ui/react";
-import Link from "next/link";
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
-import { PageLayout } from "@ui/Layout/PageLayout";
-import { Tag } from "@ui/Tag/Tag";
-import * as GridCard from "@ui/Card/GridCard";
-import moment from "moment";
+import { notFound } from "next/navigation";
+import React from "react";
+import TutorialVideoPage, { AutoProps } from "./(components)/TutorialVideoPage";
+import { getTutorialById } from "workspaces/cms-data/src/tutorials";
 
-export interface AutoProps {
-  readonly params: {
-    readonly locale: string;
-  };
-}
+export default async function TutorialVideo({ params }: AutoProps) {
+  const { locale, id } = params;
 
-interface Tutorial {
-  id: string;
-  title: string;
-  url: string;
-  tags: string[];
-  author: string;
-  difficulty: string;
-  published_at: string;
-}
-
-
-function TutorialPage({ params }: AutoProps): JSX.Element | null {
-  const [tutorial, setTutorial] = useState<Tutorial | null>(null);
-  const pathname = usePathname()!;
-  
-  useEffect(() => {
-    const fetchTutorial = async () => {
-      try {
-        const response = await axios.get<Tutorial>(`/api/getTutorialById?id=${`tutorials:uk:${pathname.split('/').pop()}.yml`}`);
-        setTutorial(response.data);
-      } catch (error) {
-        // handle error
-      }
-    };
-
-    fetchTutorial();
-  }, [pathname]);
-
-  if (!tutorial) {
-    return <div>Loading...</div>;
-  }
-  const videoId = tutorial.url.split('/').pop();
-  const embedUrl = `https://www.youtube.com/embed/${videoId}`;
-  const date = moment(tutorial.published_at).format("MMM DD, YYYY");
-  return <PageLayout
-    sectionHeaderTitle={tutorial.title}
-    breadcrumbs={
-      <Breadcrumb separator="/">
-        <BreadcrumbItem>
-          <BreadcrumbLink
-            as={Link}
-            href={`/${params.locale}`}
-            fontSize="sm"
-            noOfLines={1}
-          >
-            Home
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbItem>
-          <BreadcrumbLink
-            as={Link}
-            href={`/${params.locale}/developers`}
-            fontSize="sm"
-            noOfLines={1}
-          >
-            Developers
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbItem>
-          <BreadcrumbLink
-            as={Link}
-            href={`/${params.locale}/tutorials`}
-            fontSize="sm"
-            noOfLines={1}
-          >
-            Tutorials
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-
-        <BreadcrumbItem isCurrentPage>
-          <BreadcrumbLink fontSize="sm">{tutorial.title}</BreadcrumbLink>
-        </BreadcrumbItem>
-      </Breadcrumb>
+  try {
+    const tutorial = await getTutorialById(id, locale);
+    if (tutorial?.type !== "youtube") {
+      throw new Error("Tutorial is not a video");
     }
-    main={<div>
-      <Box width="100%" height={{base: "250px", sm: "350px", md: "500px", lg: "650px"}}>
-        <iframe width="100%" height="100%" src={embedUrl} frame-border="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      </Box>
-      <Flex mb="4" mt="4" direction="row">
-        <GridCard.Content
-          title={''}
-          author={tutorial.author}
-          date={date}
-          difficulty={tutorial.difficulty}
-          direction="row"
-        />
-        <Flex height="40px" ml="24px">
-          {Array.isArray(tutorial.tags) &&
-          tutorial.tags.map((tag, i) => {
-            // only show max 2 tags
-            if (i > 1) return null;
-            return (
-              <Tag key={i} variant="listCard">
-                {tag}
-              </Tag>
-            );
-          })}
-        </Flex>
-      </Flex>
-    </div>}/>;
+    return <TutorialVideoPage tutorial={tutorial} params={params} />;
+  } catch (error) {
+    notFound();
+  }
 }
-
-export default TutorialPage;
---
+++ workspaces/cms-data/src/tutorials.tsx
@@ -0,0 +1,37 @@
+import algoliasearch from "algoliasearch";
+
+export type Tutorial = {
+  readonly id?: string;
+  readonly type: "youtube" | "blog" | "github";
+  readonly url: string;
+  readonly image?: string;
+  readonly title: string;
+  readonly author?: string;
+  readonly published_at: string;
+  readonly difficulty?: "beginner" | "intermediate" | "advanced";
+  readonly tags?: string[];
+  readonly locale: string;
+  readonly filepath: string;
+};
+
+export const getTutorialById = async (id: string, locale: string) => {
+  const client = algoliasearch(
+    process.env.ALGOLIA_APP_ID!,
+    process.env.ALGOLIA_SEARCH_API_KEY!
+  );
+  //   const index = client.initIndex(`web_posts_${process.env.ALGOLIA_INDEX}`);
+  const index = client.initIndex(`web_tutorials_${process.env.ALGOLIA_INDEX}`);
+
+  const searchResponse = await index.search<Tutorial>("", {
+    facetFilters: [`locale:${locale}`, `id:${id}`],
+  });
+
+  const tutorial = searchResponse.hits[0];
+  console.log("tutorial", tutorial);
+
+  if (tutorial == null) {
+    throw new Error("Tutorial not found!");
+  }
+
+  return tutorial;
+};
