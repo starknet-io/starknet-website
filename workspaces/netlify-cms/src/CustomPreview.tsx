73a6dd80eb67fa842ba505d1b16069dbc3fdbbbd
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2023-05-30 17:31:46 +0500

    Use blob image for uploaded draft images (#483)

--- workspaces/netlify-cms/src/CustomPreview.tsx
+++ workspaces/netlify-cms/src/CustomPreview.tsx
@@ -20,28 +20,63 @@ export enum CustomPreviewType {
 type CustomPreviewProps = {
   entry: PreviewTemplateComponentProps["entry"];
   type: CustomPreviewType;
+  getAsset: PreviewTemplateComponentProps["getAsset"];
 };
+
+async function toDataURL(src: string) {
+  return new Promise((resolve) => {
+    var image = new Image();
+    image.crossOrigin = "Anonymous";
+    image.onload = function () {
+      var canvas = document.createElement("canvas");
+      var context = canvas.getContext("2d")!;
+      canvas.height = image.naturalHeight;
+      canvas.width = image.naturalWidth;
+      context.drawImage(image, 0, 0);
+      var dataURL = canvas.toDataURL("image/jpeg");
+      resolve(dataURL);
+    };
+    image.src = src;
+  });
+}
 
 export default function CustomPreview(props: CustomPreviewProps) {
   const ref = useRef<HTMLIFrameElement>(null);
   const [refresh, setRefresh] = React.useState(false);
   const { height } = useWindowSize();
+  const { type, entry, getAsset } = props;
 
   useEffect(() => {
-    const { entry } = props;
     let data = entry.getIn(["data"]).toJS();
-    if (props.type === CustomPreviewType.TUTORIALS) {
-      data.tags = convertStringTagsToArray(data.tags);
-    }
+    var image = entry.getIn(["data", "image"]);
+    var asset = getAsset(image);
 
-    ref.current?.contentWindow?.postMessage(
-      {
-        type: props.type,
-        payload: data,
-      },
-      "*"
-    );
-  }, [props, refresh]);
+    const sendDataToLivePreviewRendere = async () => {
+      if (image && asset?.url) {
+        if (asset.path === "empty.svg") {
+          data.image = "";
+        } else if (asset.fileObj) {
+          data.image = await toDataURL(URL.createObjectURL(asset.fileObj));
+        } else {
+          data.image = image.replace("public", "");
+        }
+      }
+
+      if (type === CustomPreviewType.TUTORIALS) {
+        data.tags = convertStringTagsToArray(data.tags);
+      }
+
+      ref.current?.contentWindow?.postMessage(
+        {
+          type,
+          payload: data,
+        },
+        "*"
+      );
+    };
+
+    sendDataToLivePreviewRendere();
+  }, [type, entry, getAsset, refresh]);
 
   return (
     <div>
