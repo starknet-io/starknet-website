a3ea0941f874715ed53bee1a43a3763e60889ae2
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2023-06-06 16:39:37 +0500

    Fix cms prefix issue

--- workspaces/netlify-cms-backend-github/src/utils.ts
+++ workspaces/netlify-cms-backend-github/src/utils.ts
@@ -1,12 +1,10 @@
+// cms - prod cms
+// dms - dev cms
+// pms - PR cms (any branch other than dev and prod)
+
 export const CMS_BRANCH_PREFIX =
   import.meta.env.VITE_GIT_BRANCH_NAME === "production"
     ? "cms"
-    : `${import.meta.env.VITE_GIT_BRANCH_NAME}-cms`;
-
-export function contentKeyFromBranch(branch: string) {
-  return branch.slice(`${CMS_BRANCH_PREFIX}/`.length);
-}
-
-export function branchFromContentKey(contentKey: string) {
-  return `${CMS_BRANCH_PREFIX}/${contentKey}`;
-}
+    : import.meta.env.VITE_GIT_BRANCH_NAME === "dev"
+    ? `dms`
+    : "pms";
