d21445788c1237396ea8149005a0ab0a4980dad1
Tsotne Nazarashvili <cotne.95@gmail.com>
2023-06-06 17:15:45 +0400

    Update utils.ts

--- workspaces/netlify-cms-backend-github/src/utils.ts
+++ workspaces/netlify-cms-backend-github/src/utils.ts
@@ -1,7 +1,7 @@
 export const CMS_BRANCH_PREFIX =
   import.meta.env.VITE_GIT_BRANCH_NAME === "production"
     ? "cms"
-    : import.meta.env.VITE_GIT_BRANCH_NAME.match(/((?:^|.*-)cms)\//)?.[1] ?? `${import.meta.env.VITE_GIT_BRANCH_NAME }-cms`;
+    : `${import.meta.env.VITE_GIT_BRANCH_NAME}-cms`;
 
 export function contentKeyFromBranch(branch: string) {
   return branch.slice(`${CMS_BRANCH_PREFIX}/`.length);
