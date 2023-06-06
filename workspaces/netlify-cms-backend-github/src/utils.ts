b2d2e6ae1038d1efba0b7f2de4f8c88b223eb6c8
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2023-06-06 16:06:50 +0500

    just testing

--- workspaces/netlify-cms-backend-github/src/utils.ts
+++ workspaces/netlify-cms-backend-github/src/utils.ts
@@ -1,12 +1,20 @@
 export const CMS_BRANCH_PREFIX =
   import.meta.env.VITE_GIT_BRANCH_NAME === "production"
     ? "cms"
-    : `${import.meta.env.VITE_GIT_BRANCH_NAME}-cms`;
+    : import.meta.env.VITE_GIT_BRANCH_NAME === "dev"
+    ? `devcms`
+    : "branchcms";
+
+// export const CMS_BRANCH_PREFIX = "cms";
 
 export function contentKeyFromBranch(branch: string) {
+  console.log("incoming branch", branch);
   return branch.slice(`${CMS_BRANCH_PREFIX}/`.length);
 }
 
 export function branchFromContentKey(contentKey: string) {
-  return `${CMS_BRANCH_PREFIX}/${contentKey}`;
+  console.log("incoming contentKey", contentKey);
+  const returnval = `${CMS_BRANCH_PREFIX}/${contentKey}`;
+  console.log("returnval", returnval);
+  return returnval;
 }
