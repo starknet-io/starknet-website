7ea5b985aaea37a637f34045d45207f469e6ddf0
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2023-06-12 17:15:20 +0500

    revert some changes

--- workspaces/cms-config/src/collections/roadmapVersions.ts
+++ workspaces/cms-config/src/collections/roadmapVersions.ts
@@ -7,7 +7,7 @@ export const roadmapVersionsCollectionConfig = {
   label_singular: "Roadmap version",
   identifier_field: "id",
   folder: "_data/roadmap-versions",
-  slug: "{{id}}",
+  slug: "{{version}}",
   summary: "{{version}} - {{impact}}",
   create: true,
   format: "yml",
