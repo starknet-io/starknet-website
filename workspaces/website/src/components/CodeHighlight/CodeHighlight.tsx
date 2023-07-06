30e83df0fb698ce0a891d5f2366c0a37f5798d3f
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2023-07-06 21:35:36 +0500

    Add cairo language support and refactor highlight logic

---
+++ workspaces/website/src/components/CodeHighlight/CodeHighlight.tsx
@@ -0,0 +1,42 @@
+import hljs from "highlight.js";
+import { CodeBlock as CodeBlockType } from "@starknet-io/cms-data/src/pages";
+import { Box, Divider, Text } from "@chakra-ui/react";
+import CopyButton from "@ui/Button/CopyButton";
+import { codeLanguages } from "@starknet-io/cms-config/src/blocks";
+
+type CodeHighlightProps = {
+  code: string
+  language: CodeBlockType['language']
+}
+
+export default function CodeHighlight(props: CodeHighlightProps) {
+  const { code, language } = props;
+  const langDetails = codeLanguages.find(c => c.value === language)
+
+  const codeHtml = code ? hljs.highlight(code, {language}).value: ''
+
+  return (
+    <Box position="relative" zIndex="0" bg="card-code-bg" role='group'>
+      <Box
+        rounded="8px"
+        m="8"
+        sx={{ "& > div": { paddingBlock: "5", paddingEnd: "4" } }}
+        overflow='auto'
+      >
+        <pre>
+          <code
+            dangerouslySetInnerHTML={{ __html: codeHtml }}
+            className={`hljs language-${language}`}
+          />
+        </pre>
+      </Box>
+      <Box position="absolute" right="1.25em" top='1rem' display="flex" alignItems='center' gap="10px" opacity={0} transition='all 0.2s cubic-bezier(.08,.52,.52,1)' _groupHover={{
+        opacity: 1
+      }}>
+        <Text fontSize='sm'>{langDetails?.abbreviation || language}</Text>
+        <Divider orientation='horizontal' height='1rem' w='1px' bg='code-divider-bg'/>
+        <CopyButton code={code} />
+      </Box>
+    </Box>
+  );
+}
