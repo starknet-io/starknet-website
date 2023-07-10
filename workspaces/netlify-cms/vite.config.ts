fc6b623ef8d2b1ed36cf1886796489c9fa0ade42
Tsotne Nazarashvili <cotne.95@gmail.com>
2023-07-10 14:08:56 +0400

    add languages.json

--- workspaces/netlify-cms/vite.config.ts
+++ workspaces/netlify-cms/vite.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import languages from './languages.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'update-netlify-cms-code-block',
+      transform(src, id) {
+        if (
+          /\.vite\/deps\/netlify-cms-app\.js/.test(id)||
+          /netlify-cms-app\/dist\/netlify-cms-app\.js/.test(id)
+        ) {
+          return {
+            code: src.replace(/\=\s*\[\s*{\s*label:\s*"AGS Script"/, `=${JSON.stringify(languages)}, _sdsd=[{label:"AGS Script"`),
+            map: null,
+          }
+        }
+      },
+    }
+  ],
   server: {
     port: 1234
-  }
+  },
 })
