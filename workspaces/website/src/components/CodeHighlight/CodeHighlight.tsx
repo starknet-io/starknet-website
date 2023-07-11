1e2be92d9940c8c69414542af7562154836deac5
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2023-07-11 23:07:03 +0500

    Add cairo mode and auto highlight when language is missing

--- workspaces/website/src/components/CodeHighlight/CodeHighlight.tsx
+++ workspaces/website/src/components/CodeHighlight/CodeHighlight.tsx
@@ -5,23 +5,29 @@ import CopyButton from "@ui/Button/CopyButton";
 import { codeLanguages } from "@starknet-io/cms-config/src/blocks";
 
 type CodeHighlightProps = {
-  code: string
-  language: CodeBlockType['language']
+  code?: string
+  language?: CodeBlockType['language']
 }
 
 export default function CodeHighlight(props: CodeHighlightProps) {
   const { code, language } = props;
   const langDetails = codeLanguages.find(c => c.value === language)
 
-  const codeHtml = code ? hljs.highlight(code, {language}).value: ''
+  let codeHtml = ''
+  if(code && language){
+    codeHtml = hljs.highlight(code, {language}).value
+  } else if(code){
+    codeHtml = hljs.highlightAuto(code).value
+  }
   const langAbbreviation = langDetails?.abbreviation || language
+  const languageClassname = language ? `hljs language-${language}`: 'hljs'
 
   return (
     <Box position="relative" zIndex="0" className="hljs-box" role='group' mb='1rem'>
         <pre>
           <code
             dangerouslySetInnerHTML={{ __html: codeHtml }}
-            className={`hljs language-${language}`}
+            className={languageClassname}
           />
         </pre>
       <Box position="absolute" right="1.25em" top='5px' display="flex" alignItems='center' gap="10px" opacity={0} transition='all 0.2s cubic-bezier(.08,.52,.52,1)' _groupHover={{
