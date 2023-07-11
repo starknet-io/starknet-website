71581a38dd0a57eacbec7d500f971e08c2100bd6
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2023-07-11 18:07:08 +0500

    Adjust code languages select

--- workspaces/website/src/components/CodeHighlight/CodeHighlight.tsx
+++ workspaces/website/src/components/CodeHighlight/CodeHighlight.tsx
@@ -14,6 +14,7 @@ export default function CodeHighlight(props: CodeHighlightProps) {
   const langDetails = codeLanguages.find(c => c.value === language)
 
   const codeHtml = code ? hljs.highlight(code, {language}).value: ''
+  const langAbbreviation = langDetails?.abbreviation || language
 
   return (
     <Box position="relative" zIndex="0" className="hljs-box" role='group' mb='1rem'>
@@ -26,8 +27,12 @@ export default function CodeHighlight(props: CodeHighlightProps) {
       <Box position="absolute" right="1.25em" top='5px' display="flex" alignItems='center' gap="10px" opacity={0} transition='all 0.2s cubic-bezier(.08,.52,.52,1)' _groupHover={{
         opacity: 1
       }}>
-        <Text fontSize='sm'>{langDetails?.abbreviation || language}</Text>
-        <Divider orientation='horizontal' height='1rem' w='1px' bg='code-divider-bg'/>
+        {langAbbreviation && (
+          <>
+            <Text fontSize='sm'>{langAbbreviation}</Text>
+            <Divider orientation='horizontal' height='1rem' w='1px' bg='code-divider-bg'/>
+          </>
+         )}
         <CopyButton code={code} />
       </Box>
     </Box>
