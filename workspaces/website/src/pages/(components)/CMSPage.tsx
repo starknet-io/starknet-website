b9fbb17559db0f780c85fb2702a9f753982d5b0f
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2023-07-17 23:34:46 +0500

    Extract toc logic and use same function in blogs

--- workspaces/website/src/pages/(components)/CMSPage.tsx
+++ workspaces/website/src/pages/(components)/CMSPage.tsx
@@ -1,11 +1,8 @@
-import type { Page as PageType, TopLevelBlock } from "@starknet-io/cms-data/src/pages";
+import type { Page as PageType } from "@starknet-io/cms-data/src/pages";
 import { PageLayout } from "@ui/Layout/PageLayout";
 import moment from "moment";
-import remarkParse from "remark-parse";
 import { Block } from "src/blocks/Block";
-import { unified } from "unified";
-import { Index } from "unist-util-index";
-import { TableOfContents } from "../(components)/TableOfContents";
+import { TableOfContents } from "./TableOfContents/TableOfContents";
 import {
   Box,
   Breadcrumb,
@@ -14,6 +11,7 @@ import {
   Flex,
 } from "@chakra-ui/react";
 import '@ui/CodeHighlight/code-highlight-init'
+import { blocksToTOC } from "./TableOfContents/blocksToTOC";
 
 type CMSPageProps = {
   data: PageType;
@@ -84,97 +82,10 @@ export default function CMSPage({
         }
         rightAside={
           data.template === "content" ? (
-            <TableOfContents headings={pageToTableOfContents(data.blocks, 1)} />
+            <TableOfContents headings={blocksToTOC(data.blocks, 1)} />
           ) : null
         }
       />
     </Box>
   );
-}
-
-interface HeadingData {
-  readonly title: string;
-  readonly level: number;
-}
-
-function pageToTableOfContents(blocks: readonly TopLevelBlock[] = [], level: number, tableOfContents: HeadingData[] = []): readonly HeadingData[] {
-  blocks.forEach((block) => {
-    if(block.type === 'page_header'){
-      return
-    }
-    else if (block.type === "container") {
-      pageToTableOfContents(block.blocks, level, tableOfContents);
-    } else if (block.type === "flex_layout" || block.type === "group") {
-      if (!block.heading) {
-        pageToTableOfContents(block.blocks, level, tableOfContents);
-      } else {
-        const headingData: HeadingData = {
-          title: block.heading,
-          level,
-        };
-        tableOfContents.push(headingData);
-        pageToTableOfContents(block.blocks, level + 1, tableOfContents);
-      }
-    } else if (block.type === "ordered_block") {
-      const sortedBlocks = Array.from(block.blocks || []).sort((a, b) => {
-        return a.title.localeCompare(b.title);
-      });
-
-      const blockItems: HeadingData[] = sortedBlocks.map((block) => {
-        return {
-          title: block.title,
-          level
-        };
-      });
-
-      tableOfContents.push(...blockItems);
-    } else if (block.type === "accordion") {
-      if (block.heading != null) {
-        const headingData: HeadingData = {
-          title: block.heading,
-          level
-        };
-        tableOfContents.push(headingData);
-      }
-    } else if (block.type === "markdown") {
-      const processor = unified()
-        .use(remarkParse)
-        .use(() => {
-          return (tree: any) => {
-            const typeIndex = new Index("type", tree);
-            const headings = typeIndex.get("heading");
-
-            const headingItems: HeadingData[] = headings.map((node: any) => {
-              const textNode = node.children.find((n: any) => {
-                return n.type === "text";
-              });
-
-              return {
-                title: textNode?.value ?? "",
-                level
-              };
-            });
-
-            tableOfContents.push(...headingItems);
-          };
-        });
-
-      const node = processor.parse(block.body);
-      processor.runSync(node);
-    } else if ("title" in block) {
-      const headingData: HeadingData = {
-        title: block.title,
-        level
-      };
-      tableOfContents.push(headingData);
-    } else if ("heading" in block && block.heading != null) {
-      const headingData: HeadingData = {
-        title: block.heading,
-        level
-      };
-      tableOfContents.push(headingData);
-    }
-  });
-
-  return tableOfContents;
 }
