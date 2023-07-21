a0ea9d8220bec6af89c24e459c8903d53ba76791
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2023-07-21 20:40:44 +0500

    Improve youtubeVideoIdFromURL and add a test for it

--- workspaces/cms-utils/src/index.ts
+++ workspaces/cms-utils/src/index.ts
@@ -5,11 +5,14 @@ export function youtubeVideoIdFromURL(url: string): string | undefined | void {
   try {
     const obj = new URL(url);
 
-    if (obj.hostname === "www.youtube.com") {
+    if (obj.hostname === "www.youtube.com" || obj.hostname === "youtube.com") {
       if (obj.searchParams.get("v")) return obj.searchParams.get("v")!;
 
       if (obj.pathname.startsWith("/live/")) {
         return obj.pathname.replace("/live/", "");
+      }
+      if(obj.pathname.startsWith("/embed/")) {
+        return obj.pathname.replace("/embed/", "");
       }
     } else if (obj.hostname === "youtu.be") {
       return obj.pathname.slice(1);
