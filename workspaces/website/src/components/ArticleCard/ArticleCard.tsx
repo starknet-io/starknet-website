f743f7120476ea8002992d42e27cd39fd62f8b95
Milos Lekovic <milos@yuki.wtf>
2023-07-21 15:13:20 +0200

    Image resizing on posts and tutorials page

--- workspaces/website/src/components/ArticleCard/ArticleCard.tsx
+++ workspaces/website/src/components/ArticleCard/ArticleCard.tsx
@@ -7,6 +7,7 @@ import {
   Icon,
   Flex,
   ChakraProps,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { Text } from "@ui/Typography/Text";
 import { Heading } from "@ui/Typography/Heading";
@@ -17,13 +18,18 @@ import { Category as DataCategory } from "@starknet-io/cms-data/src/categories";
 type RootProps = {
   children: React.ReactNode;
   href: string;
-  type?: | "grid" | "featured";
-  sx?: ChakraProps['sx']
+  type?: "grid" | "featured";
+  sx?: ChakraProps["sx"];
 };
 
 const Root = ({ children, href, type = "grid", sx }: RootProps) => {
   return (
-    <CardGradientBorder padding="0" borderRadius={{ base: "8px" }} overflow="hidden" sx={sx}>
+    <CardGradientBorder
+      padding="0"
+      borderRadius={{ base: "8px" }}
+      overflow="hidden"
+      sx={sx}
+    >
       <Box as="a" href={href} _hover={{ textDecor: "none" }} role="group">
         <Box p="0" height="full" borderRadius="8px" bg="card-bg">
           <Flex
@@ -32,7 +38,11 @@ const Root = ({ children, href, type = "grid", sx }: RootProps) => {
             // justify="space-between"
             height={type === "featured" ? "400px" : "full"}
           >
-            <Flex gap={type === "featured" ? "1" : "8"} direction={type === "featured" ? "row" : "column"} flex={1}>
+            <Flex
+              gap={type === "featured" ? "1" : "8"}
+              direction={type === "featured" ? "row" : "column"}
+              flex={1}
+            >
               {children}
             </Flex>
           </Flex>
@@ -45,19 +55,43 @@ const Root = ({ children, href, type = "grid", sx }: RootProps) => {
 type ImageProps = {
   url?: string;
   imageAlt?: string;
-  type?: | "grid" | "featured";
+  type?: "grid" | "featured";
 };
 
 const Image = ({ url, imageAlt, type = "grid" }: ImageProps) => {
+  const size = useBreakpointValue({
+    base: "581px",
+    sm: "350px",
+    md: "430px",
+    xl: "320px",
+  });
+  const featuredImageSize = useBreakpointValue({
+    base: "581px",
+    sm: "350px",
+    md: "430px",
+    lg: "550px",
+    xl: "606px",
+  });
+  const cloudflareImage = `https://starknet.io/cdn-cgi/image/width=${
+    type === "featured" ? featuredImageSize : size
+  },height=auto,format=auto${url}`;
+  const isProd = import.meta.env.VITE_ALGOLIA_INDEX !== "production";
   return (
-    <Box overflow="hidden" {...type === "featured" && { width: "auto", maxWidth: "60%"}}>
+    <Box
+      overflow="hidden"
+      {...(type === "featured" && { width: "auto", maxWidth: "60%" })}
+    >
       <ChakraImage
-        src={url}
+        src={isProd ? cloudflareImage : url}
         alt={imageAlt}
         width="full"
-        height={type === "featured" ? "100%" : { base: "16rem", md: "12rem", lg: "10rem" }}
+        height={
+          type === "featured"
+            ? "100%"
+            : { base: "16rem", md: "12rem", lg: "10rem" }
+        }
         objectFit="cover"
-        {...type === "grid" && { borderTopRadius: 8}}
+        {...(type === "grid" && { borderTopRadius: 8 })}
       />
     </Box>
   );
@@ -65,12 +99,18 @@ const Image = ({ url, imageAlt, type = "grid" }: ImageProps) => {
 
 type BodyProps = {
   children: React.ReactNode;
-  type?: | "grid" | "featured";
+  type?: "grid" | "featured";
 };
 
 const Body = ({ children, type = "grid" }: BodyProps) => {
   return (
-    <Flex flex={1} direction="column" pl={6} pr={6} {...type === "featured" && { pt: 8 }}>
+    <Flex
+      flex={1}
+      direction="column"
+      pl={6}
+      pr={6}
+      {...(type === "featured" && { pt: 8 })}
+    >
       {children}
     </Flex>
   );
@@ -93,7 +133,7 @@ const Category = ({ category }: CategoryProps) => {
 type ContentProps = {
   title: string;
   excerpt: string;
-  type?: | "grid" | "featured";
+  type?: "grid" | "featured";
 };
 
 const Content = ({ title, excerpt, type = "grid" }: ContentProps) => {
@@ -104,7 +144,10 @@ const Content = ({ title, excerpt, type = "grid" }: ContentProps) => {
         variant={type === "featured" ? "h2" : "h4"}
         fontWeight="bold"
         noOfLines={type === "featured" ? 3 : 2}
-        {...type === "featured" && { fontSize: "2.7rem", lineHeight: "3.025rem" }}
+        {...(type === "featured" && {
+          fontSize: "2.7rem",
+          lineHeight: "3.025rem",
+        })}
       >
         {title}
       </Heading>
@@ -119,13 +162,13 @@ type FooterProps = {
   postType: string;
   publishedAt?: string;
   timeToConsume?: string;
-  type?: | "grid" | "featured";
+  type?: "grid" | "featured";
 };
 const Footer = ({
   postType,
   publishedAt = "N/A",
   timeToConsume = "5min read",
-  type = "grid"
+  type = "grid",
 }: FooterProps) => {
   const renderPostTypeIcon = () => {
     switch (postType) {
