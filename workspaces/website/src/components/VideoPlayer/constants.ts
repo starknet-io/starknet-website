cfb4300f3bf2f3643da3a9303ca2e5243959a17c
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2023-08-13 23:46:45 +0500

    Video player ui tweaks && increase video quality filter

--- workspaces/website/src/components/VideoPlayer/constants.ts
+++ workspaces/website/src/components/VideoPlayer/constants.ts
@@ -10,7 +10,7 @@ export const playlist: Chapter[] = [
   {
     id: "scaling-eth",
     title: "How Starknet scales Ethereum",
-    description: "Episode 1 explains how Starknet works at a high level",
+    description: "Chapter 1 explains how Starknet works at a high level",
     thumbnail: "/assets/video/chapter1.jpg",
     videoId: "abd99c9696f1862f828c6e18136d75e5",
     durationTime: "02:42",
@@ -18,7 +18,7 @@ export const playlist: Chapter[] = [
   {
     id: "sequencer",
     title: "The Starknet Sequencer",
-    description: "Episode 2 explains about the Starknet Sequencer",
+    description: "Chapter 2 explains about the Starknet Sequencer",
     thumbnail: "/assets/video/chapter2.jpg",
     videoId: "7ca4a89a8031a1737e4632ec498afb55",
     durationTime: "02:24",
@@ -26,7 +26,7 @@ export const playlist: Chapter[] = [
   {
     id: "prover",
     title: "The Starknet Prover",
-    description: "Episode 3 explains how the Starknet Prover works",
+    description: "Chapter 3 explains how the Starknet Prover works",
     thumbnail: "/assets/video/chapter3.jpg",
     videoId: "b66a934468bf72606e60abd7d475acf3",
     durationTime: "02:26",
@@ -34,7 +34,7 @@ export const playlist: Chapter[] = [
   {
     id: "eth-settlement",
     title: "Secure settlement on Ethereum",
-    description: "Episode 5 explains about how to secure settlement on Ethereum",
+    description: "Chapter 4 explains how to secure settlement on Ethereum",
     thumbnail: "/assets/video/chapter4.jpg",
     videoId: "af654c93367a0d4eae7ea90252ac342e",
     durationTime: "03:28",
