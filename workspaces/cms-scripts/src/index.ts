9d592fa6617a7c1a6b05b889391dc7dddf07a55d
lorcan-codes <126797224+lorcan-codes@users.noreply.github.com>
2023-08-18 12:32:25 +0500

    Create path if not exists

--- workspaces/cms-scripts/src/index.ts
+++ workspaces/cms-scripts/src/index.ts
@@ -151,6 +151,7 @@ const createSocialMediaData = async () => {
           followersCount: discordData.approximate_member_count,
       }
 
+      await fs.mkdir(`public/data/social-media`, { recursive: true });
       await write(
         path.join("public/data/social-media", "data.json"),
         {
@@ -177,7 +178,6 @@ const createSocialMediaData = async () => {
             followersCount: 172000,
           }
         }
-        await fs.mkdir(`public/data/social-media`, { recursive: true });
         await write(
           path.join("public/data/social-media", "data.json"),
           staticData
