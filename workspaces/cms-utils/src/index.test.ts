96801fb851e8a3bed43b22787afd7debf4a00f5c
momodaka <463435681@qq.com>
2023-11-14 00:07:15 +0800

    test: add unit test fir cms-utils

--- workspaces/cms-utils/src/index.test.ts
+++ workspaces/cms-utils/src/index.test.ts
@@ -1,5 +1,5 @@
-import { youtubeVideoIdFromURL } from './index'; // replace with your actual file path
-import { expect, test } from 'vitest';
+import {expect, test, describe } from 'vitest';
+import {convertStringTagsToArray, youtubeVideoIdFromURL, getShuffledArray } from './index';
 
 test('youtubeVideoIdFromURL', () => {
   // Testing full YouTube URLs
@@ -13,7 +13,7 @@ test('youtubeVideoIdFromURL', () => {
 
   // Testing no 'www'
   expect(youtubeVideoIdFromURL('https://youtube.com/live/NL-skz5VW_0')).toBe('NL-skz5VW_0');
-  
+
   // Testing shortened URLs
   expect(youtubeVideoIdFromURL('https://youtu.be/dQw4w9WgXcQ')).toBe('dQw4w9WgXcQ');
 
@@ -21,3 +21,55 @@ test('youtubeVideoIdFromURL', () => {
   expect(youtubeVideoIdFromURL('https://www.youtube.com/embed/FWvP5bh0yIM')).toBe('FWvP5bh0yIM');
 
 });
+
+describe('convertStringTagsToArray test', () => {
+  test('should convert a comma-separated string of tags to an array', () => {
+    const inputTags = 'tag1, tag2, tag3';
+    const result = convertStringTagsToArray(inputTags);
+    expect(result).toEqual(['tag1', 'tag2', 'tag3']);
+  });
+
+  test('should trim whitespace around tags', () => {
+    const inputTags = ' tag1     , tag2 , tag3 ';
+    const result = convertStringTagsToArray(inputTags);
+
+    expect(result).toEqual(['tag1', 'tag2', 'tag3']);
+  });
+});
+
+describe('getShuffledArray test', () => {
+
+  const inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  test('should return an array of the same length as the input', () => {
+    const result = getShuffledArray(inputArray);
+
+    expect(result).toHaveLength(inputArray.length);
+  });
+
+  test('should return an array with the same elements as the input', () => {
+    const result = getShuffledArray(inputArray);
+
+    inputArray.forEach((element) => {
+      expect(result).toContain(element);
+    });
+  });
+
+  test('should not modify the original array', () => {
+    const originalArray = [...inputArray];
+    const result = getShuffledArray(inputArray);
+
+    expect(inputArray).toEqual(originalArray);
+    expect(inputArray).not.toEqual(result);
+  });
+
+
+
+  test('should produce different results on different invocations', () => {
+    const result1 = getShuffledArray(inputArray);
+    const result2 = getShuffledArray(inputArray);
+
+    expect(result1).not.toEqual(result2);
+  });
+});
+
