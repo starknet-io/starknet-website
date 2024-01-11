152f4596c45d61cf0dbe40cf199db829e2079bcd
PedroBailaAndrade <pedrocbva@gmail.com>
2024-01-11 15:48:45 +0000

    Add video to sitemap

--- workspaces/cms-scripts/src/utils.ts
+++ workspaces/cms-scripts/src/utils.ts
@@ -1,5 +1,6 @@
 import fs from "fs/promises";
 import YAML from "yaml";
+import { format } from 'date-fns';
 
 export async function json(filepath: string): Promise<any> {
   return JSON.parse(await fs.readFile(filepath, { encoding: "utf8" }));
@@ -46,3 +47,36 @@ export async function getFirst<T>(...fns: Array<() => Promise<T>>): Promise<T> {
     cause,
   });
 }
+
+/**
+ * Escape xml function.
+ */
+
+export const escapeXml = (string: string) => {
+  return string.replace(/[<>&'"]/g, (char) => {
+    switch (char) {
+      case '<': return '&lt;';
+      case '>': return '&gt;';
+      case '&': return '&amp;';
+      case '\'': return '&apos;';
+      case '"': return '&quot;';
+      default: return char;
+    }
+  });
+}
+
+/**
+ * Format date function.
+ */
+
+export const formatDate = (date: string) => {
+  const newDate = new Date(date);
+  const offset = -newDate.getTimezoneOffset();
+  const offsetSign = offset >= 0 ? '+' : '-';
+  const offsetHours = String(Math.floor(Math.abs(offset) / 60)).padStart(2, '0');
+  const offsetMinutes = String(Math.abs(offset) % 60).padStart(2, '0');
+
+  const formatedDate = format(newDate, "yyyy-MM-dd'T'HH:mm:ss") + `${offsetSign}${offsetHours}:${offsetMinutes}`;
+
+  return formatedDate;
+};
