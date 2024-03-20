87ceebde12bd05a9fd8590e00ad24885e164414d
Tal-Ben-Avi <talb@webiya.co.il>
2024-03-20 11:19:53 +0200

    delete unused text

--- workspaces/website/src/components/SideStickyBanner/SideStickyBanner.tsx
+++ workspaces/website/src/components/SideStickyBanner/SideStickyBanner.tsx
@@ -1,22 +1,6 @@
 import { Box, Heading } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
-// const array = [
-//   {
-//     title: "What are onramps and cross rollup bridges?",
-//     buttonText: "see more",
-//     link: "",
-//     image:
-//       "https://s3-alpha-sig.figma.com/img/4f25/c2db/258212229ef560d9d1532f86afece898?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MNRLkOJFL7kX7dw3SFz9~oJT5-lOeiAaUN2K~hkoyIWl7~ywwXn-7H4QFYTPnfI2xuPlwFWq2PwE4nS13bCD--1LU9fn7Qvj5tVwQH2C4412u1KvAyDArz1B7RQKLISYU799PhMVyPHrMUJ875RGJJrG515m~b~pzoes0zLE96qkYJxdliOxXTbH0C5kz2ceD~er-6eUCx7eYeMWBWLgwgmIwpsxXx8-aceuuMzz9O6rN54fhiGMjMpXF-YfMdQSi1Mmng2q3lwH7pIAgRxgMlXFSZHRnea3VNm5gkL6PBtjdUx8kaROpx7ryVcYBiGuRe5Olvji9rLl8rqeUa9bdA__",
-//   },
-//   {
-//     title: "What are onramps and cross rollup bridges?",
-//     buttonText: "see more",
-//     link: "",
-//     image:
-//       "https://s3-alpha-sig.figma.com/img/4f25/c2db/258212229ef560d9d1532f86afece898?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MNRLkOJFL7kX7dw3SFz9~oJT5-lOeiAaUN2K~hkoyIWl7~ywwXn-7H4QFYTPnfI2xuPlwFWq2PwE4nS13bCD--1LU9fn7Qvj5tVwQH2C4412u1KvAyDArz1B7RQKLISYU799PhMVyPHrMUJ875RGJJrG515m~b~pzoes0zLE96qkYJxdliOxXTbH0C5kz2ceD~er-6eUCx7eYeMWBWLgwgmIwpsxXx8-aceuuMzz9O6rN54fhiGMjMpXF-YfMdQSi1Mmng2q3lwH7pIAgRxgMlXFSZHRnea3VNm5gkL6PBtjdUx8kaROpx7ryVcYBiGuRe5Olvji9rLl8rqeUa9bdA__",
-//   },
-// ];
 interface NavbarStickyBannerProps {
   children: ReactNode;
 }
