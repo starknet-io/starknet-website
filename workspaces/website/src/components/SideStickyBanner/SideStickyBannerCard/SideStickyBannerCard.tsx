f370816f5e43eb36447a13a12713b49d8b31722f
Tal-Ben-Avi <talb@webiya.co.il>
2024-03-20 14:02:38 +0200

    adding gtm event

--- workspaces/website/src/components/SideStickyBanner/SideStickyBannerCard/SideStickyBannerCard.tsx
+++ workspaces/website/src/components/SideStickyBanner/SideStickyBannerCard/SideStickyBannerCard.tsx
@@ -14,13 +14,19 @@ const SideStickyBannerCard = ({
   buttonText,
   buttonLink,
 }: LatestAnnouncementCardProps) => {
+  const gtmEvent = (target: string) =>
+    window.gtag?.("event", target, { event_category: "engagement" });
+  const onReadMore = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    gtmEvent("Latest_announcement_read_more");
+  };
   return (
     <Box mb={6}>
       <Image src={image} alt={image} borderRadius={8} />
       <Text fontSize="14px" fontWeight={600} mt={3} mb={2}>
         {text}
       </Text>
-      <Link href={buttonLink} color="Link">
+      <Link href={buttonLink} color="Link" onClick={onReadMore}>
         {buttonText} <ArrowForwardIcon boxSize="14px" mb={0.5} />
       </Link>
     </Box>
